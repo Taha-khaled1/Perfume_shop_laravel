bd45612fdb8606c8b1d06695daf6ed587fc13ffb
Taha khalid <tth31770@gmail.com>
2023-04-25 02:28:00 +0200

    first commit

--- resources/views/site/homePage/homePage.blade.php
+++ resources/views/site/homePage/homePage.blade.php
@@ -72,9 +72,13 @@
           
 
             @if (LaravelLocalization::getCurrentLocaleDirection() == 'rtl')
-            <h1>{{$category->name }}</h1>
+            <a href="{{route('category_property',$category->id)}}"><h1>{{$category->name }}</h1></a>
+
+            
         @else
-        <h1>{{$category->name_en }}</h1>
+        
+        <a href="{{route('category_property',$category->id)}}"><h1>{{$category->name_en }}</h1></a>
+
         @endif
             <!-- Swiper -->
             @if (LaravelLocalization::getCurrentLocaleDirection() == 'rtl')
