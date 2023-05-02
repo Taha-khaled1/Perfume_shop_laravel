27871d64df51a88cc33a7e7e1082046807947a52
Taha khalid <tth31770@gmail.com>
2023-05-02 19:06:21 +0300

    first commit

--- resources/views/site/auth/registerPage.blade.php
+++ resources/views/site/auth/registerPage.blade.php
@@ -54,29 +54,23 @@
                         </div>
 
 
-
                         <div class="mb-3 row">
-                            <label   class="col-sm-3 col-form-label text-end"> {{__('Principality')}}</label>
+                            <div class="col-lg-3 col-form-label text-end">{{__('Principality')}}</div>
                             <div class="col-lg-9 col-xl-8">
-    
-                                <select class="form-control" name="city_id">
-                                    @foreach ($city as $item)
-                                        <option value="{{ $item->id }}">
-    
-                                            @if (LaravelLocalization::getCurrentLocaleDirection() == 'rtl')
-                                                {{ $item->name }}
-                                            @else
-                                                {{ $item->name_en }}
-                                            @endif
-                                        </option>
-                                    @endforeach
-    
-    
-    
-                                </select>
+                              <select class="form-control" name="city_id" style="width: 100%;">
+                                @foreach ($city as $item)
+                                <option value="{{ $item->id }}">
+                                  @if (LaravelLocalization::getCurrentLocaleDirection() == 'rtl')
+                                  {{ $item->name }}
+                                  @else
+                                  {{ $item->name_en }}
+                                  @endif
+                                </option>
+                                @endforeach
+                              </select>
                             </div>
-                        </div>
-
+                          </div>
+                          
 
 
 
